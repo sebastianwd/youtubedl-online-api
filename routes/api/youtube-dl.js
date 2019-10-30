c7dde7493581162b293dd1503d084b0403f5c53b
Sebastian <sebastian.luque.pl@gmail.com>
2019-10-29 22:22:46 -0500

    test

--- routes/api/youtube-dl.js
+++ routes/api/youtube-dl.js
@@ -26,16 +26,37 @@ router.get("/audio", function(req, res) {
     const videos = results.videos;
     const url = "https://www.youtube.com" + videos[0].url;
 
-    youtubedl.exec(
-      url,
-      ["-f", "bestaudio", "-g", "--geo-bypass-country", "PE"],
-      {},
-      function(err, output) {
-        if (err) throw err;
-        res.json(output);
-      }
-    );
+    console.log("url: " + url);
+    youtubedl.exec(url, ["-f", "bestaudio", "-g", "--geo-bypass"], {}, function(
+      err,
+      output
+    ) {
+      if (err) throw err;
+      res.json(output);
+    });
   });
+});
+
+router.get("/", function(req, res) {
+  try {
+    console.log("url: " + req.query.url);
+    console.log("options: " + req.query.o);
+
+    const url = req.query.url;
+    let options = req.query.o;
+
+    if (!Array.isArray(options)) {
+      options = [options];
+    }
+
+    youtubedl.exec(url, options || [], {}, function(err, output) {
+      if (err) throw err;
+      res.json(output);
+    });
+  } catch (error) {
+    res.json("Couldn't process that command");
+    console.warn(error);
+  }
 });
 
 module.exports = router;
